7596bb78e8c26dd6cb1d8cd642e3ea6e1ab3aedd
adolf <yomingyo@gmail.com>
2021-03-26 00:51:12 +0800

    update

--- binance.py
+++ binance.py
@@ -1,2 +1,7 @@
 # ！/usr/bin/env python
 # -*- coding:utf-8 -*-
+import ccxt
+
+
+"API key:e3cDWMh8N1uugwePjZK0OLZ73dMCl45kX7kIbniN9kjx42r5UtBAGs1S6JKvEXiu"
+"secret key:F6OShDNksFqTqCqD8mGbAEmi7sDubGWxHakra3nA8xVn3RWbw9qsDqNMi75OhNVG"
