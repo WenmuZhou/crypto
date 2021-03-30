ae91e3c6bbc3b527c2ee608bd0e57899d16ebbe5
Adolf <adolf1321794021@gmail.com>
2021-03-30 15:13:14 +0800

    update

--- strategy/ema_strategy.py
+++ strategy/ema_strategy.py
@@ -7,22 +7,66 @@ import talib
 import numpy as np
 import pandas as pd
 
-coin_name = "BTC"
-history_df = pd.read_csv("dataset/day/" + coin_name + ".csv")
+trade_rate = 1.5 / 1000
 
-ma_len = 10
-history_df["EMA"] = talib.EMA(history_df["close"], timeperiod=ma_len)
-history_df["SMA"] = talib.SMA(history_df["close"], timeperiod=ma_len)
 
-# print(history_df)
-start_new = {"USDT": 100,
-             coin_name: 0}
+def ema_test(coin_name_, ma_len=10, mode="SMA"):
+    df = pd.read_csv("dataset/day/" + coin_name_ + ".csv")
 
-print(len(history_df))
+    df["EMA"] = talib.EMA(df["close"], timeperiod=ma_len)
+    df["SMA"] = talib.SMA(df["close"], timeperiod=ma_len)
 
-for index, row in history_df.iterrows():
-    # print(row)
-    pass
-# print(start_new)
-# print("币价值自身变化", history_df['close'][len(history_df) - 1] / history_df['open'][0])
-# print("最终剩余价值", (row["close"] * start_new[coin_name] + start_new["USDT"]) / 100)
+    df.dropna(subset=['EMA', 'SMA'], inplace=True)
+
+    df['coin_pct'] = df['close'].pct_change(1)
+
+    df.loc[df['close'] > df[mode], 'style'] = "buy"
+    df.loc[df['close'] < df[mode], 'style'] = "sell"
+
+    df['pos'] = df['style'].shift(1)
+
+    del df['style']
+
+    df.dropna(subset=['pos'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    df.loc[df['pos'] != df['pos'].shift(1), 'trade_time'] = df['time_stamp']
+
+    if df.loc[0, 'pos'] == "sell":
+        df.loc[0, "trade_time"] = None
+
+    # # 将调仓日的涨跌幅修正为开盘价买入涨跌幅
+    df.loc[(df['trade_time'].notnull()) & (df['pos'] == 'buy'), 'strategy_pct'] = df['close'] / (
+            df['open'] * (1 + trade_rate)) - 1
+
+    df.loc[df['trade_time'].isnull() & (df['pos'] == 'buy'), 'strategy_pct'] = df['coin_pct']
+    df.loc[df['trade_time'].isnull() & (df['pos'] == 'sell'), 'strategy_pct'] = 0
+    df.loc[(df['trade_time'].notnull()) & (df['pos'] == 'sell'), 'strategy_pct'] = -trade_rate
+    # # 扣除卖出手续费
+    # df.loc[(df['trade_time'].shift(-1).notnull()), 'strategy_pct_adjust'] = (1 + df[
+    #     'strategy_pct']) * (1 - trade_rate) - 1
+    df['coin_net'] = (1 + df['coin_pct']).cumprod()
+    df['strategy_net'] = (1 + df['strategy_pct']).cumprod()
+
+    coin_net_ = df.loc[len(df) - 1, "coin_net"]
+    strategy_net_ = df.loc[len(df) - 1, "strategy_net"]
+
+    return coin_net_, strategy_net_
+
+
+coin_list = ["BNB", "BTC", "DOT", "EOS", "ETH", "FIL", "LTC", "XRP"]
+ma_list = [3, 5, 10, 20, 30, 50, 60, 90, 120, 240, 360]
+mode_list = ["SMA", "EMA"]
+
+fin_list = []
+for coin_name in coin_list:
+    for ma_len in ma_list:
+        for mode_type in mode_list:
+            coin_net, strategy_net = ema_test(coin_name_="BTC", ma_len=10, mode="SMA")
+            # print(coin_net, strategy_net)
+            res = [coin_name, ma_len, mode_type, coin_net, strategy_net, strategy_net > coin_net]
+            fin_list.append(res)
+
+res_df = pd.DataFrame(fin_list)
+res_df.to_csv("result/单均线策略测试.csv")
+print(res_df)
