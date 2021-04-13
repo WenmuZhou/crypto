153310fcb9d52319cb46c758170e1bf9640d8abe
Adolf <adolf1321794021@gmail.com>
2021-04-13 19:20:58 +0800

    update

--- get_data/get_kdata.py
+++ get_data/get_kdata.py
@@ -1,6 +1,7 @@
 # ！/usr/bin/env python
 # -*- coding:utf-8 -*-
 import ccxt
+import os
 import pandas as pd
 
 exchange = ccxt.binance()
@@ -9,34 +10,42 @@ exchange = ccxt.binance()
 # exchange.secret = "F6OShDNksFqTqCqD8mGbAEmi7sDubGWxHakra3nA8xVn3RWbw9qsDqNMi75OhNVG"
 #
 coin_list = ["BTC", "ETH", "EOS", "FIL", "LTC", "XRP", "DOT", "FIL", "KSM", "CAKE", "BNB", "ADA", "UNI"]
-time_period = "1h"
-
+time_period = "15m"
+range_number = 20
+limit_number = 1000
 
 # coin_list = ["BTC"]
 
 
-# start_time = 1616016600000
-# time_diff = 899100000
 def get_exchange_data(coin_name):
     res_data = []
-    for i in range(10):
+    for i in range(range_number):
         if len(res_data) == 0:
-            data = exchange.fetch_ohlcv(coin_name + "/USDT", timeframe=time_period, limit=1000)
-            time_dff = data[-1][0] - data[0][0]
+            data = exchange.fetch_ohlcv(coin_name + "/USDT", timeframe=time_period, limit=limit_number)
+            # print('data len', len(data))
+            time_dff = data[1][0] - data[0][0]
+            # print(time_dff)
         else:
+            # print(res_data)
+            # print(len(res_data))
             st = res_data[0][0]
             # et = res_data[-1][0]
-            # print('st',res_data[0][0])
+            # print('st', res_data[0][0])
             # print('et',res_data[-1][0])
-            data = exchange.fetch_ohlcv(coin_name + "/USDT", timeframe=time_period, limit=1000, since=st - time_dff)
+            data = exchange.fetch_ohlcv(coin_name + "/USDT", timeframe=time_period, limit=limit_number,
+                                        since=st - limit_number * time_dff)
         res_data = data + res_data
     # print(res_data)
-    print(len(res_data))
+    # print(len(res_data))
     df = pd.DataFrame(res_data, columns=["time", "open", "high", "low", "close", "vol"])
     df['time_stamp'] = pd.to_datetime(df["time"], unit="ms")
-    print(df)
+    # print(df)
     df.drop_duplicates(subset=['time_stamp'], inplace=True)
-    df.to_csv("dataset/1hour/" + coin_name + ".csv", index=False)
+    print(len(df))
+    data_dir = os.path.join("dataset", time_period)
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+    df.to_csv(os.path.join(data_dir, coin_name + ".csv"), index=False)
 
 
 for coin_name_ in coin_list:
--- get_data/handle_data.py
+++ get_data/handle_data.py
@@ -2,9 +2,10 @@
 # -*- coding:utf-8 -*-
 import os
 
-for path, dir_list, file_list in os.walk("dataset"):
-    print(file_list)
-    for file in file_list:
-        if ".csv" in file:
-            new_name = file.split(".")[0].upper()
-            os.rename(os.path.join(path, file), os.path.join(path, new_name + ".csv"))
+# 批量修改名字
+# for path, dir_list, file_list in os.walk("dataset"):
+#     print(file_list)
+#     for file in file_list:
+#         if ".csv" in file:
+#             new_name = file.split(".")[0].upper()
+#             os.rename(os.path.join(path, file), os.path.join(path, new_name + ".csv"))
