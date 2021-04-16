53d5730c0c7a112bdfeba720fc0cdaee2f954522
朱韬南 <zhutaonan@zhutaonandeMacBook-Pro.local>
2021-04-16 22:51:48 +0800

    update

--- trading/turn_mul.py
+++ trading/turn_mul.py
@@ -51,11 +51,8 @@ def auto_trade_v2(coin_list):
             trick = exchange.fetch_ticker(symbol=max_value_coin + "/USDT")
             exchange.create_limit_sell_order(symbol=max_value_coin + "/USDT", price=trick["bid"],
                                              amount=balance_my[max_value_coin])
-<<<<<<< HEAD
+
             balance_my_new, max_value_coin_new, balance_my_value = get_balance_info(coin_list, exchange)
-=======
-            balance_my_new, max_value_coin_new,balance_my_value = get_balance_info(coin_list, exchange)
->>>>>>> b7be1f9369da5602c0fcabca39be8ccae9c043a6
 
         if now_style != "USDT":
             trick = exchange.fetch_ticker(symbol=now_style + "/USDT")
