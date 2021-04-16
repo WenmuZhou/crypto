b7be1f9369da5602c0fcabca39be8ccae9c043a6
朱韬南 <zhutaonan@zhutaonandeMacBook-Pro.local>
2021-04-16 20:28:08 +0800

    update

--- strategy/turtle.py
+++ strategy/turtle.py
@@ -23,19 +23,29 @@ df['lower_band'] = talib.MIN(df.low, timeperiod=20).shift(1)
 
 
 # df["Date"] = df["time_stamp"]
-df.rename(columns={"time_stamp": "Date", "vol": "volume"}, inplace=True)
-
-df.set_index("Date", inplace=True)
-df.index = pd.to_datetime(df.index)
+# df.rename(columns={"time_stamp": "Date", "vol": "volume"}, inplace=True)
+#
+# df.set_index("Date", inplace=True)
+# df.index = pd.to_datetime(df.index)
 
 df.loc[df["close"] > df["upper_band"], "style"] = "BTC"
 df.loc[df['close'] < df["lower_band"], "style"] = "empty"
 
-my_color = mpf.make_marketcolors(up="red", down="green", edge="inherit", volume="inherit")
-my_style = mpf.make_mpf_style(marketcolors=my_color)
+df['coin_pct'] = df['close'].pct_change(periods=1)
 
-add_plot = [mpf.make_addplot(df[['upper_band', 'lower_band']])]
-mpf.plot(df, type="candle", ylabel="price(usdt)", style=my_style, volume=True, ylabel_lower="vol",
-         addplot=add_plot)
+df["pos"] = df["style"].shift(1)
+df["pos"] = df["pos"].fillna(method='pad')
 
-df.to_csv("tmp/test2.csv", index=False)
+df.dropna(inplace=True)
+del df["style"]
+# df.loc[df['pos'] != df['pos'].shift(1), 'trade_time'] = df["time_stamp"]
+# df.loc[df["pos"]=="BTC","strategy_pct"] = df["coin_pct"]
+
+
+# my_color = mpf.make_marketcolors(up="red", down="green", edge="inherit", volume="inherit")
+# my_style = mpf.make_mpf_style(marketcolors=my_color)
+
+# add_plot = [mpf.make_addplot(df[['upper_band', 'lower_band']])]
+# mpf.plot(df, type="candle", ylabel="price(usdt)", style=my_style,addplot=add_plot)
+print(df)
+# df.to_csv("tmp/test2.csv", index=False)
