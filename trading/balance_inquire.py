edea06b82371f0b26029de913ced4b7ba41da866
Adolf <adolf1321794021@gmail.com>
2021-04-16 14:14:06 +0800

    update

--- trading/balance_inquire.py
+++ trading/balance_inquire.py
@@ -14,9 +14,11 @@ exchange = ccxt.binance()
 coin_list = ["BTC", "ETH", "EOS", "XRP", "DOT", "BNB", "ADA", "UNI"]
 
 api_key_dict = \
-    {"wenmu": "J0p53QWHzOaU6h7ZmmGukFfJ7C97tN3rhhs7s3jFmZJ2rNHZvxYvoYDHklMrWWZq"}
+    {"wenmu": "J0p53QWHzOaU6h7ZmmGukFfJ7C97tN3rhhs7s3jFmZJ2rNHZvxYvoYDHklMrWWZq",
+     "nan": "e3cDWMh8N1uugwePjZK0OLZ73dMCl45kX7kIbniN9kjx42r5UtBAGs1S6JKvEXiu"}
 api_secret_dict = \
-    {"wenmu": "0MOMZJC3fNW0FsDL5Xu3qj2YNK8dPVqDgbxqR3USCi396uy1aCXxW2Tto78nuGWA"}
+    {"wenmu": "0MOMZJC3fNW0FsDL5Xu3qj2YNK8dPVqDgbxqR3USCi396uy1aCXxW2Tto78nuGWA",
+     "nan": "F6OShDNksFqTqCqD8mGbAEmi7sDubGWxHakra3nA8xVn3RWbw9qsDqNMi75OhNVG"}
 
 for api_name, api_key in api_key_dict.items():
     api_secret = api_secret_dict[api_name]
@@ -29,4 +31,3 @@ for api_name, api_key in api_key_dict.items():
         datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         api_name, balance_my_value * 6.72),
                          token="8392f247561974cf01f63efc77bfeb814c70a00453aee8eb26c405081af03dbe")
-
