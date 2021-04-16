ae5aa7b29f01296b2f9069afd00331f35df4539d
Adolf <adolf1321794021@gmail.com>
2021-04-16 18:23:12 +0800

    update

--- strategy/turtle.py
+++ strategy/turtle.py
@@ -5,3 +5,37 @@
 # @Author   : Adolf
 # @File     : turtle.py
 # @Function  :
+import talib
+import pandas as pd
+import numpy as np
+import mplfinance as mpf
+
+pd.set_option("expand_frame_repr", False)
+pd.set_option("display.max_rows", 1000)
+
+df = pd.read_csv("dataset/1d/BTC.csv")
+# print(df)
+
+df['upper_band'] = talib.MAX(df.high, timeperiod=20).shift(1)
+df['lower_band'] = talib.MIN(df.low, timeperiod=20).shift(1)
+# df['upper_band'] = df["upper_band"].shift(-1)
+# df['lower_band'] = df["lower_band"].shift(-1)
+
+
+# df["Date"] = df["time_stamp"]
+df.rename(columns={"time_stamp": "Date", "vol": "volume"}, inplace=True)
+
+df.set_index("Date", inplace=True)
+df.index = pd.to_datetime(df.index)
+
+df.loc[df["close"] > df["upper_band"], "style"] = "BTC"
+df.loc[df['close'] < df["lower_band"], "style"] = "empty"
+
+my_color = mpf.make_marketcolors(up="red", down="green", edge="inherit", volume="inherit")
+my_style = mpf.make_mpf_style(marketcolors=my_color)
+
+add_plot = [mpf.make_addplot(df[['upper_band', 'lower_band']])]
+mpf.plot(df, type="candle", ylabel="price(usdt)", style=my_style, volume=True, ylabel_lower="vol",
+         addplot=add_plot)
+
+df.to_csv("tmp/test2.csv", index=False)
