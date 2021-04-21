cc5825909f305dacb262e4582f103ed7862a37ad
Adolf <adolf1321794021@gmail.com>
2021-04-21 20:26:09 +0800

    update

--- trading/trade_execution/time_1d.py
+++ trading/trade_execution/time_1d.py
@@ -14,4 +14,4 @@ auto_trade.trading_main(coin_list=["BTC", "ETH"], user="nan", time_periods="1d",
 auto_trade.trading_main(coin_list=["BTC", "ETH"], user="shuig", time_periods="1d", momentum_days=20)
 
 auto_trade_v2 = TurtleTrade()
-auto_trade_v2.trading_main(coin_name="BTC", user="yujl", upper_band=30, lower_band=20)
+auto_trade_v2.trading_main(coin_name="BTC", user="yujl", upper_band=30, lower_band=20, time_periods="1d")
