bc94be9b3ef2e333dbebbaec383f9fc1d4be1520
Adolf <adolf1321794021@gmail.com>
2021-05-23 11:58:53 +0800

    修改30分钟币池
    Conflicts:
    	trading/trade_execution/time_30min.py

---
+++ trading/trade_execution/time_30min.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project  : crypto
+# @Time     : 2021/5/19 10:56
+# @Author   : Adolf
+# @File     : time_30min.py
+# @Function  :
+from trading.trade_strategy.turn_trade import TurnTrade
+
+auto_trade = TurnTrade()
+try:
+    auto_trade.trading_main(coin_list=["BTC", "ETH", "FIL", "BAT", "ADA", "DOT", "CAKE", "UNI"],
+                            user="nan",
+                            time_periods="30m",
+                            momentum_days=46)
+except Exception as e:
+    print(e)
+    print('nan bug')
