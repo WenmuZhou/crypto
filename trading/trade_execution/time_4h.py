de831a858b8387bc28f4e782a8a8cb38ad5869ff
Adolf <adolf1321794021@gmail.com>
2021-05-24 21:32:10 +0800

    优化30分钟和4小时币池

--- trading/trade_execution/time_4h.py
+++ trading/trade_execution/time_4h.py
@@ -10,7 +10,7 @@ from trading.trade_strategy.turn_trade import TurnTrade
 auto_trade = TurnTrade()
 try:
     auto_trade.trading_main(
-        coin_list=["DOT", "KSM", "UNI", "CAKE", "BAKE", "FIL", "MATIC", "BCH", "LINK", "BAT", "LTC", "EOS", "DOGE"],
+        coin_list=["DOT", "KSM", "UNI", "CAKE", "BAKE", "FIL", "MATIC", "BCH", "LINK", "BAT", "LTC", "ETH", "DOGE"],
         user="wxt",
         time_periods="4h",
         momentum_days=5)
