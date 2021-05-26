a517f9c60a31e30bfacbf4e502e2c002c44856c2
Adolf <adolf1321794021@gmail.com>
2021-05-26 20:13:54 +0800

    update

--- strategy/personalise/loop_pos/ma_wind.py
+++ strategy/personalise/loop_pos/ma_wind.py
@@ -21,27 +21,27 @@ class MaWind(TradeStructure):
         self.data['ma10'] = self.data["close"].rolling(10).mean()
 
         self.data.loc[(self.data["ma5"] > self.data["ma10"]) & (
-                self.data["ma5"].shift(1) < self.data["ma10"].shift(1)), "trade"] = "b"
+                self.data["ma5"].shift(1) <= self.data["ma10"].shift(1)), "trade"] = "buy"
         self.data.loc[(self.data["ma5"] < self.data["ma10"]) & (
-                self.data["ma5"].shift(1) > self.data["ma10"].shift(1)), "trade"] = "s"
+                self.data["ma5"].shift(1) >= self.data["ma10"].shift(1)), "trade"] = "sell"
 
-        self.data['diff'] = self.data.apply(lambda x: (x.ma5 - x.ma10) / min(x.ma5, x.ma10), axis=1)
-        self.data["area"] = 0
-        area_ma = 0
-        area_ma_list = []
-        for index, row in self.data.iterrows():
-            # print(row)
-            self.data.loc[index, "area"] = area_ma
-            if row["trade"] != "b" and row["trade"] != "s":
-                area_ma += row["diff"]
-            else:
-                area_ma_list.append(area_ma)
-                area_ma = row["diff"]
-        df2 = self.data[(self.data["trade"] == "s") | (self.data["trade"] == "b")].copy()
-        df2.reset_index(inplace=True)
-        df2["pct"] = df2["close"].pct_change(periods=1).shift(-1)
-        df3 = df2[df2["trade"] == "b"].copy()
-        print(df3)
+        # self.data['diff'] = self.data.apply(lambda x: (x.ma5 - x.ma10) / min(x.ma5, x.ma10), axis=1)
+        # self.data["area"] = 0
+        # area_ma = 0
+        # area_ma_list = []
+        # for index, row in self.data.iterrows():
+        #     # print(row)
+        #     self.data.loc[index, "area"] = area_ma
+        #     if row["trade"] != "buy" and row["trade"] != "sell":
+        #         area_ma += row["diff"]
+        #     else:
+        #         area_ma_list.append(area_ma)
+        #         area_ma = row["diff"]
+        # df2 = self.data[(self.data["trade"] == "s") | (self.data["trade"] == "b")].copy()
+        # df2.reset_index(inplace=True)
+        # df2["pct"] = df2["close"].pct_change(periods=1).shift(-1)
+        # df3 = df2[df2["trade"] == "b"].copy()
+        # print(df3)
         # df4 = df3[abs(df3["area"]) < 0.2].copy()
         # df3['origin_pct'] = (1 + df3['pct']).cumprod()
         # df4['adjust_pct'] = (1 + df4['pct']).cumprod()
@@ -51,5 +51,6 @@ class MaWind(TradeStructure):
 
 
 if __name__ == '__main__':
-    mawind = MaWind(data_path="dataset/stock/600570.csv")
-    mawind.cal_technical_index()
+    mawind = MaWind(data_path="dataset/stock/600570_post.csv")
+    # mawind.cal_technical_index()
+    mawind(analyze_positions=True)
--- strategy/show_trade_html/parse_show_data.py
+++ strategy/show_trade_html/parse_show_data.py
@@ -11,6 +11,7 @@ from strategy.personalise.loop_pos.ma_wind import MaWind
 
 def parse_data(stock_id="600570", time_period="day", start_time="", end_time=""):
     day_data_path = "/root/adolf/dataset/stock/d_pre/"
+    min_data_base_path = "/root/adolf/dataset/stock/jy_data/"
     if time_period == "day":
         if os.path.exists(os.path.join(day_data_path, "sz." + stock_id + ".csv")):
             df_path = os.path.join(day_data_path, "sz." + stock_id + ".csv")
@@ -18,12 +19,23 @@ def parse_data(stock_id="600570", time_period="day", start_time="", end_time="")
             df_path = os.path.join(day_data_path, "sh." + stock_id + ".csv")
         else:
             return {"error_msg": "不存在股票代码"}
+    elif "min" in time_period:
+        min_data_path = os.path.join(min_data_base_path,time_period)
+        if os.path.exists(os.path.join(min_data_path, stock_id + ".SZ.csv")):
+            df_path = os.path.join(min_data_path, stock_id + ".SZ.csv")
+        elif os.path.exists(os.path.join(min_data_path, stock_id + ".SH.csv")):
+            df_path = os.path.join(min_data_path, stock_id + ".SH.csv")
+        else:
+            return {"error_msg": "不存在股票代码"}
     # print(df)
-    print(df_path)
+    else:
+        return {"error_msg": "不存在的时间周期"}
+
+    # print(df_path)
     show_json = MaWind(data_path=df_path)(show_buy_and_sell=True)
     # print(show_json)
     return show_json
 
 
 if __name__ == '__main__':
-    parse_data(stock_id="600570", level="day")
+    parse_data(stock_id="600570", time_period="30min")
