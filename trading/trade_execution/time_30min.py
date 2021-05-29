90bc88439b2e6ef99892715c8bbb2914814df3fe
Adolf <adolf1321794021@gmail.com>
2021-05-29 14:59:55 +0800

    计算成交量加权移动平均线

--- trading/trade_execution/time_30min.py
+++ trading/trade_execution/time_30min.py
@@ -9,10 +9,12 @@ from trading.trade_strategy.turn_trade import TurnTrade
 
 auto_trade = TurnTrade()
 try:
-    auto_trade.trading_main(coin_list=["BTC", "ETH", "FIL", "BAT", "ADA", "DOT", "CAKE", "UNI", "MATIC", "KSM", "XRP"],
-                            user="nan",
-                            time_periods="30m",
-                            momentum_days=30)
+    auto_trade.trading_main(
+        coin_list=['BTC', 'ETH', 'EOS', 'FIL', 'LTC', 'ETC', 'BCH', 'BAT', 'XRP', 'DOT', 'KSM', 'BNB', 'LINK', 'ADA',
+                   'UNI', 'CHZ', 'DOGE', 'MATIC'],
+        user="nan",
+        time_periods="30m",
+        momentum_days=47)
 except Exception as e:
     print(e)
     print('nan bug')
