40380e3b007cd87244bb447b80cecb5da493fc8b
Adolf <adolf1321794021@gmail.com>
2021-06-09 08:51:28 +0800

    update

--- strategy/personalise/portfolio/cnm_strategy.py
+++ strategy/personalise/portfolio/cnm_strategy.py
@@ -5,6 +5,19 @@
 # @Author   : Adolf
 # @File     : cnm_strategy.py
 # @Function  :
+import os
 import pandas as pd
 
-stock_pool = []
+pd.set_option("expand_frame_repr", False)
+
+stock_dir = "dataset/stock/stock_handle/"
+stock_pool = os.listdir(stock_dir)
+# print(stock_pool)
+
+for stock in stock_pool:
+    # print(stock)
+    if stock != "600570.csv":
+        continue
+    stock_path = os.path.join(stock_dir, stock)
+    stock_df = pd.read_csv(stock_path)
+    print(stock_df)
