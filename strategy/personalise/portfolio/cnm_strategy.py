7b774b9bc5bcde551d7246167cfa0463243c968e
Adolf <adolf1321794021@gmail.com>
2021-06-13 17:43:00 +0800

    update

--- strategy/personalise/portfolio/cnm_strategy.py
+++ strategy/personalise/portfolio/cnm_strategy.py
@@ -8,26 +8,24 @@
 import os
 import pandas as pd
 
+from functools import reduce
+
 pd.set_option("expand_frame_repr", False)
 
 stock_dir = "dataset/stock/stock_handle/"
 stock_pool = os.listdir(stock_dir)
 # print(stock_pool)
 
-min_len = 99999
-code_id = ""
+data_frames = []
 for stock in stock_pool:
-    # print(stock)
-    # if stock != "600570.csv":
-    #     continue
     stock_path = os.path.join(stock_dir, stock)
     stock_df = pd.read_csv(stock_path)
     stock_df["code"] = stock.split(".csv")[0]
 
-    if len(stock_df) < min_len:
-        min_len = len(stock_df)
-        code_id = stock
-    # print(stock_df)
+    data_frames.append(stock_df)
 
-# print(min_len)
-# print(code_id)
+df_merged = reduce(lambda left, right: pd.merge(left, right, on=['date'],
+                                                how='outer'), data_frames)
+df_merged.sort_values(by=['date'], inplace=True)
+
+print(df_merged)
